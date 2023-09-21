a6a7fda4bc408bc00de6706d52d5a13707a6b707
rioadrian24 <rioadrian646@gmail.com>
2023-09-21 08:11:00 +0700

    first commit

---
+++ AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
@@ -0,0 +1,21 @@
+package id.ac.polinema.androidlifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	// TODO: tambahkan callback onStart() di sini
+
+
+	// TODO: tambahkan callback onStop() di sini
+
+	// TODO: lengkapi callback untuk onRestart() onResume() onPause() onDestroy()
+}
